8fadb07164fb73f92a6de918adc510dc96f14372
sagia <sagia@rapyd.net>
2020-09-25 16:46:36 +0300

    Adds firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpjYiMwwymwwdscl-LJuTtbSzfbVHIICg",
+  authDomain: "plantime-7391f.firebaseapp.com",
+  databaseURL: "https://plantime-7391f.firebaseio.com",
+  projectId: "plantime-7391f",
+  storageBucket: "plantime-7391f.appspot.com",
+  messagingSenderId: "220554975860",
+  appId: "1:220554975860:web:0aabc9068275273a8f0f21",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
